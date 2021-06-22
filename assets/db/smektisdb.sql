b853789afac6348ea52c3b323f8921ff9b031b8d
unknown <mohamadnurriduwan@gmail.com>
2021-06-22 11:17:45 +0700

    tambah db baru

--- assets/db/smektisdb.sql
+++ assets/db/smektisdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2021 at 02:33 AM
+-- Generation Time: Jun 22, 2021 at 06:16 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -44,13 +44,10 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id`, `user_id`, `kategori_id`, `kode_barang`, `nama`, `harga`, `stok`, `keterangan`, `foto`) VALUES
-(1, 1, 1, NULL, 'Canon EOS 60D', 125000, 1, 'Boleh dipinjam kapan saja', '0'),
-(2, 1, 1, NULL, 'Sony A7ii', 250000, 5, 'Boleh dipinjam kapan saja', '0'),
-(11, 1, 2, NULL, 'Godox', 80000, 4, 'Lampu Flash', NULL),
-(12, 1, 2, NULL, 'LED 500 Watt', 150000, 3, 'lampu Terang benerang', NULL),
-(59, 1, 6, NULL, 'ximatec', 15000, 1, 'biasa', NULL),
-(60, 1, 1, 'CN001', 'Canon100D', 150000, 1, 'asdda', NULL),
-(64, 1, 1, 'CN002', 'EOS 70D', 300000, 4, 'wwww', NULL);
+(66, 1, 11, 'SNY001', 'Sony A7ii', 300000, 1, 'Kamera Sony A7ii Dari Pusbang Film', NULL),
+(67, 1, 11, 'CN001', 'Canon EOS 70D', 150000, 1, 'Kamera Canon 70D', NULL),
+(68, 1, 11, 'CN002', 'Canon EOS 60D', 150000, 1, 'Kamera Canon 60D', NULL),
+(69, 1, 12, 'GIM001', 'Zhiyun Crane 3 Labs', 350000, 1, 'Gimbal Zhiyun Crane 3 Labs', NULL);
 
 -- --------------------------------------------------------
 
@@ -70,20 +67,10 @@ CREATE TABLE `barang_pinjam` (
 --
 
 INSERT INTO `barang_pinjam` (`id`, `transaksi_id`, `barang_id`, `jumlah`) VALUES
-(20, 20, 2, 2),
-(22, 21, 11, 1),
-(23, 21, 12, 1),
-(24, 22, 12, 2),
-(25, 23, 2, 1),
-(26, 24, 11, 1),
-(27, 25, 2, 1),
-(28, 26, 1, 1),
-(29, 26, 2, 1),
-(30, 27, 59, 1),
-(32, 29, 1, 1),
-(33, 29, 2, 4),
-(35, 31, 2, 1),
-(40, 33, 11, 1);
+(49, 38, 66, 1),
+(50, 38, 67, 1),
+(51, 39, 68, 1),
+(52, 39, 69, 1);
 
 -- --------------------------------------------------------
 
@@ -93,19 +80,17 @@ INSERT INTO `barang_pinjam` (`id`, `transaksi_id`, `barang_id`, `jumlah`) VALUES
 
 CREATE TABLE `kategori` (
   `id` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL
+  `nama` varchar(50) NOT NULL,
+  `kode_kategori` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `kategori`
 --
 
-INSERT INTO `kategori` (`id`, `nama`) VALUES
-(4, 'Bahan Makanan'),
-(3, 'Gimbal'),
-(1, 'Kamera'),
-(2, 'Lighting'),
-(6, 'tripod');
+INSERT INTO `kategori` (`id`, `nama`, `kode_kategori`) VALUES
+(11, 'Kamera', 'KAM11'),
+(12, 'Gimbal', 'GIM12');
 
 -- --------------------------------------------------------
 
@@ -130,12 +115,11 @@ CREATE TABLE `penyewa` (
 --
 
 INSERT INTO `penyewa` (`id`, `user_id`, `kode_penyewa`, `nama`, `alamat`, `email`, `no_telp`, `media_sosial`, `jenis_kelamin`) VALUES
-(4, 1, NULL, 'Riduwan', 'Jl. Tumapel Singosari', 'riduwan@gmail.com', '+62895366446634', 'Ridz Creator', 'Laki-laki'),
-(5, 1, NULL, 'Sahabul Ashfari', 'Jl. Rogonoto Timur no.235', 'ahmarsahab13@gmail.com', '089680895777', 'Sahabul Ashfari', 'Laki-laki'),
-(6, 1, NULL, 'Mansur', 'Dsn Gunung Rejo Kreweh', 'rusnammildcv47@gmail.com', '081335369331', 'mansurzip', 'Laki-laki'),
-(8, 1, NULL, 'bpk wedha', 'pakis', 'wedha@gmail.com', '087775242588', 'wedha', 'Laki-laki'),
-(9, 1, NULL, 'riza', 'jl sulfat', 'riza@gmail', '9788133', 'mansurzip', 'Laki-laki'),
-(12, 1, 'BAM12', 'bambang', 'jl kramat gg mawar hitam', 'artusima7@gmail.com', '6281335369331', 'bambangcuy', 'Laki-laki');
+(12, 1, 'BAM12', 'bambang', 'jl kramat gg mawar hitam', 'artusima7@gmail.com', '6281335369331', 'bambangcuy', 'Laki-laki'),
+(14, 1, 'RID14', 'Riduwan', 'Jl Tumapel VIII No 14 Singosari Malang', 'mohamadnurriduwan@gmail.com', '0895366446634', 'NRidz.me', 'Laki-laki'),
+(15, 1, 'MAN15', 'Mansur', 'Jl Kreweh Gunungrejo', 'mansurzip@gmail.com', '081335369331', 'rusnammild', 'Laki-laki'),
+(16, 1, 'SAH16', 'Sahabul Ashfari', 'Jl Rogonoto', 'ahmarsahab13@gmail.com', '089680895777', 'ahmarsahab', 'Laki-laki'),
+(17, 1, 'WED17', 'Weda', 'Jl Pakis', 'weda@gmail.com', '	087775242588', 'weda', 'Laki-laki');
 
 -- --------------------------------------------------------
 
@@ -145,6 +129,7 @@ INSERT INTO `penyewa` (`id`, `user_id`, `kode_penyewa`, `nama`, `alamat`, `email
 
 CREATE TABLE `status` (
   `id` int(11) NOT NULL,
+  `kode_status` varchar(15) DEFAULT NULL,
   `nama` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -152,9 +137,9 @@ CREATE TABLE `status` (
 -- Dumping data for table `status`
 --
 
-INSERT INTO `status` (`id`, `nama`) VALUES
-(1, 'Pinjam'),
-(2, 'Sewa');
+INSERT INTO `status` (`id`, `kode_status`, `nama`) VALUES
+(4, 'PIN4', 'Pinjam'),
+(5, 'SEW5', 'Sewa');
 
 -- --------------------------------------------------------
 
@@ -179,17 +164,8 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id`, `user_id`, `penyewa_id`, `status_id`, `tanggal_pinjam`, `durasi`, `jaminan`, `tanggal_kembali`, `foto`) VALUES
-(20, 1, 4, 2, '2021-05-05 21:41:00', 2, 'KTP,BPJS', '2021-05-09 22:12:01', NULL),
-(21, 1, 5, 2, '2021-04-14 22:36:00', 1, 'KTP,BPJS', '2021-05-09 22:12:05', NULL),
-(22, 1, 4, 2, '2020-05-08 21:03:00', 1, 'BPJS', '2021-05-09 22:12:09', NULL),
-(23, 1, 4, 2, '2021-06-16 22:19:00', 2, 'KTP,BPJS', '2021-05-19 23:30:09', NULL),
-(24, 1, 5, 1, '2021-05-11 22:21:00', 2, 'BPJS', '2021-05-19 23:30:04', NULL),
-(25, 1, 5, 2, '2021-05-12 17:14:00', 2, 'KTP KK', '2021-05-19 23:30:01', NULL),
-(26, 1, 4, 2, '2021-05-19 23:30:00', 1, 'KTP KK', '2021-06-10 09:22:42', NULL),
-(27, 1, 6, 2, '2021-06-10 09:21:00', 2, 'KTP', NULL, NULL),
-(29, 1, 8, 2, '2021-06-10 09:33:00', 3, 'KTP', '2021-06-10 09:50:44', NULL),
-(31, 1, 8, 1, '2021-06-10 09:36:00', 1, 'KTP', '2021-06-10 09:42:20', NULL),
-(33, 1, 6, 2, '2021-06-11 19:23:00', 1, 'KTP', NULL, NULL);
+(38, 1, 16, 5, '2021-06-18 19:54:00', 2, 'KTP KK', NULL, NULL),
+(39, 1, 15, 5, '2021-06-18 19:54:00', 1, 'KTP KK', '2021-06-18 19:55:19', NULL);
 
 -- --------------------------------------------------------
 
@@ -279,37 +255,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `barang_pinjam`
 --
 ALTER TABLE `barang_pinjam`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `penyewa`
 --
 ALTER TABLE `penyewa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `status`
 --
 ALTER TABLE `status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `user`
